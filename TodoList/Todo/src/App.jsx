a4f1f26bb4af1c2a613a9dfc900b52db0869c85e
Bhanyu <whohuseynovabhanyu10@gmail.com>
2023-11-27 01:11:57 +0400

    gfghg

---
+++ TodoList/Todo/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import TodoList from './components/Todo/Todo'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+
+function App() {
+ return(
+  <>
+ <TodoList/>
+  </>
+ )
+}
+
+export default App
