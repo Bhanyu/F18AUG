0769e8898afa27a3bdb805d109cee0ef21fa591b
Bhanyu <whohuseynovabhanyu10@gmail.com>
2024-04-25 23:16:58 +0400

    kapital

---
+++ kapital/app/about/page.jsx
@@ -0,0 +1,12 @@
+
+
+const About = ()=>{
+    return(
+        <>
+        
+        <h1 className="about-title">Salammm Bura Haqqinda Sehifesidir</h1>
+        
+        </>
+    )
+}
+export default About
---
+++ kapital/app/not-found.js
@@ -0,0 +1,13 @@
+
+const NotFound = ()=>{
+    return(
+
+        <>
+        
+      <h1 className="h-24">
+      404 - Not Found
+      </h1>
+        </>
+    )
+}
+export default NotFound
