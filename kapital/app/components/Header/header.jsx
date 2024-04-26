7260c7aac416323a95346ad6129d7c414917b680
Bhanyu <whohuseynovabhanyu10@gmail.com>
2024-04-27 00:56:32 +0400

    headers

--- kapital/app/components/Header/header.jsx
+++ kapital/app/components/Header/header.jsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
-import angle from "../../../public/angle.svg";
-// import { usePathname } from "next/navigation";
+import { FaPhoneAlt } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
+import { FaAngleDown } from "react-icons/fa6";
+import me from "../../../public/down.svg"
+
 import "../Header/header.scss";
 const Header = () => {
   const navLinks = [
@@ -13,6 +16,7 @@ const Header = () => {
     { name: "Dayaniqli Inkisaf Hesabatlari", href: "/sustainability" },
     { name: "Arasdirma", href: "/arasdirma" },
     { name: "Satinalmalar", href: "/satinalmalar" },
+    {name : "Karyera Portali", href:"/karyera"}
   ];
 
   return (
@@ -27,26 +31,34 @@ const Header = () => {
                 </Link>
               );
             })}
+        <div className="select_individual">
+        <select name="private" id="private">
+          <option  value="Fərdi">Fərdi</option>
+          <option value="Korporativ">Korporativ</option>
+          <option value="KOB" data-url="/KOB">KOB</option>
+        </select>
+        </div>
+          
           </div>
           <div className="right_part_tinnies">
             <a className="search" href="#">
-              <Image
-                src="https://abb-bank.az/img/icons/search.svg"
-                width={20}
-                height={20}
-                alt="Picture of the author"
-              />
+       <Image className="research"
+       src="https://abb-bank.az/img/icons/search.svg"
+       width={12}
+       height= {12}
+       />
               <span>Axtarış</span>
             </a>
 
             <div className="select_box">
               <div className="lang">
               <span className="label">AZ</span>
-                         <button>
-<Image
-src={angle}/>
-                         </button>
-                         
+                         <Image className="angle"
+                         src={me}
+                         width={10}
+                         height={10}
+                         />
+                   
              </div>
              <div className="hide_options">
                   <div className="first_opt">
@@ -57,7 +69,7 @@ src={angle}/>
                   />
                     <option value="EN">EN</option>
                   </div>
-                  <div className="second_opt">
+                  <div className="first_opt">
                   <Image
                     src="https://abb-bank.az/img/icons/flag-ru.svg"
                     height={24}
@@ -65,7 +77,18 @@ src={angle}/>
                   />
                     <option value="EN">EN</option>
                   </div>
-                </div>
+                </div>   
+            
+            </div>
+
+            <div className="call_phone">
+             
+                <Image className="phone_icon"
+                src="https://abb-bank.az/img/icons/phone.svg"
+                width={12}
+                height={12}
+                />
+              <span>937</span>
             </div>
           </div>
         </div>
