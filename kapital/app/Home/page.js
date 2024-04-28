33df487774d5cb7322b8b00db0137a0a91dd5ffb
Bhanyu <whohuseynovabhanyu10@gmail.com>
2024-04-28 10:38:20 -0300

    kapitall

--- kapital/app/Home/page.js
+++ kapital/app/Home/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import "../Home/style.scss";
 
 export default function Home() {
   return (
--- kapital/app/components/Header/header.jsx
+++ kapital/app/components/Header/header.jsx
@@ -4,7 +4,8 @@ import { FaPhoneAlt } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
 import { FaAngleDown } from "react-icons/fa6";
 import me from "../../../public/down.svg"
-
+import logo from "../../../public/logo.svg"
+import internetbank from "../../../public/internetbank.svg"
 import "../Header/header.scss";
 const Header = () => {
   const navLinks = [
@@ -18,10 +19,30 @@ const Header = () => {
     { name: "Satinalmalar", href: "/satinalmalar" },
     {name : "Karyera Portali", href:"/karyera"}
   ];
-
+  const services = [
+    {name: " Fərdi", href:"#ferdi"},
+    {name: "Korporativ ", href:"#korporativ"},
+    {name: " KOB", href:"#KOB"}
+  ]
+const lowlinks = [
+  {name: "Kreditlər", href:"#kreditler" },
+  {name: "Kartlar", href:"#kartlar" },
+  {name: "Onlayn xidmətlər", href:"#onlaynservice" },
+  {name: "Kampaniyalar", href:"#kampaniya" },
+  {name: "Keşbek", href:"#kesbek"},
+  {name:"İpoteka", href:"#ipotek"},
+  {name: "Pul köçürmələri", href:"#pulkocur"},
+  {name : "Cari Hesab", href:"#carihesab"},
+  {name: "Əmanətlər", href:"#emanet"},
+  {name: "Azərbaycan Alış-veriş Festivalı", href:"#alisveris"}
+                        
+                        
+                        
+                    
+]
   return (
     <header>
-      <div className="upper-wrap">
+      <section id="upper-wrap">
         <div className="container">
           <div className="links_left_part">
             {navLinks.map((link) => {
@@ -92,7 +113,52 @@ const Header = () => {
             </div>
           </div>
         </div>
+      </section>
+      <section id="middle_parts">
+      <div className="container">
+      <div className="left_field_part">
+          <div className="logo">
+           <a className="abb_logo" href="#">
+           <Image
+            src={logo}
+            width={110}
+            height={50}
+            />
+           </a>
+          </div>
+          <div className="services">
+{services.map((service)=>{
+  return(
+    <Link className="service"  href={service.href} key={service.name}>
+{service.name}
+    </Link>
+  )
+})}
+          </div>
+        </div>
+        <div className="right_field_part">
+         <Link className="internet_bank" href="#">
+         <Image
+         src={internetbank}
+         />
+         </Link>
+         <span>İnternet Bankçılıq</span>
+        </div>
       </div>
+      </section>
+      <section id="low_part">
+        <div className="container">
+          <div className="low-navigation">
+{lowlinks && lowlinks.map((lowlink)=>{
+  return(
+    <Link className="low_nav_link" href={lowlink.href} key={lowlink.name}>
+  {lowlink.name}
+  </Link>
+  )
+})}
+          </div>
+        </div>
+      </section>
     </header>
   );
 };
